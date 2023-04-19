f30a476d09d206b5cd2d89a04fd754571cda1698
Eric Fragoso <eric.fragoso@gmail.com>
2023-04-19 16:36:29 -0300

    Ajuste comit de ativo

--- db/migrations/20230418100333_add-osNumero-to-ativo.ts
+++ db/migrations/20230418100333_add-osNumero-to-ativo.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('ativo', (table) => {
-    table.text('osNumero').after('qr').index().notNullable();
+    table.text('osNumero').after('qr').index();
   })
 }
 
